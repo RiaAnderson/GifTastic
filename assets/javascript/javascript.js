dd1dc2c25b745d44d43398c9d7f55ca2ae513859
Ria <astoriaa92@gmail.com>
2019-09-03 15:43:04 -0700

    Added topics array of animals.

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,5 +1,6 @@
 // Button fucntion
 // Button array
+var topics = ["Panda", "Dog", "Guinea Pig", "Penguin", "Dolphin", "Dinosaur", "Bunny", "Flamingo"];
 // Function to add additional buttons to array
 // Gif URL
 // Gif parameters
